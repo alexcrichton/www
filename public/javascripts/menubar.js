65d8f8ee9dabe69ed0e0250e76787c98fd3e889c
Alex Crichton <adcrichton@gmail.com>
2009-03-29 21:47:08 -0500

    remove unnecessary javascript, I'll add back if I really need to...

--- public/javascripts/menubar.js
+++ public/javascripts/menubar.js
@@ -10,17 +10,16 @@ function initMenus() {
 
 function showSubmenu() {
   var element = $(this).find('ul:first')
-  var item = $(this);
   if (element.length == 0)
     return;
   element.slideDown('fast'); // have to show for the offsetParent to be set for some reason...
-  var l = item.offset().left - element.offsetParent().offset().left;
-  var t = item.offset().top - element.offsetParent().offset().top;
-  if (item.hasClass('top')) {
+  var l = $(this).offset().left - element.offsetParent().offset().left;
+  var t = $(this).offset().top - element.offsetParent().offset().top;
+  if ($(this).hasClass('top')) {
     element.css('left', l + 'px');
-    element.css('top', (t + item.height()) + 'px');
+    element.css('top', (t + $(this).height()) + 'px');
   } else {
-    element.css('left', (l + item.width()) + 'px');
+    element.css('left', (l + $(this).width()) + 'px');
     element.css('top', t + 'px');
   }
 }
