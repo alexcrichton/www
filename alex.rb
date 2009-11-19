e7ab38f78312761a91f504d9102c7ebd847f6cdb
Alex Crichton <alex@alexcrichton.com>
2009-11-19 01:52:33 -0500

    surprise

--- alex.rb
+++ alex.rb
@@ -3,6 +3,7 @@ gem 'sinatra'
 gem 'haml'
 require 'sinatra'
 require 'haml'
+require 'erb'
 
 get '/' do
   haml :index
@@ -10,4 +11,12 @@ end
 
 get '/index.html' do
   haml :index
+end
+
+get '/surprise' do
+  if params[:a] == 'alexdc'
+    erb :surprise
+  else
+    'Sorry, try again!'
+  end
 end
