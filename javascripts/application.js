084b0c0b43a1638a46824bfb0775eaf69cfd3dcb
Alex Crichton <alex@alexcrichton.com>
2010-10-24 17:06:03 -0400

    Just touch up things here and there

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,19 +1,7 @@
-$(function() {
-  $('#name > a:first, #java > a:first, #websites > a:first').click(function() {
+jQuery(function($) {
+  $('#name > a:first, #java > a:first, #websites > a:first').click(function(e) {
     $('#container').attr('class', $(this).parent().attr('id'));
+    e.preventDefault();
     return false;
-  });
-
-  $('ul a').hover(function() {
-    var img = new Image();
-    var fileName = $(this).attr('id');
-
-    $(img).bind('load', function(){
-      $('#name').css('content', 'url(/images/' + fileName + '.jpg)');
-    });
-    img.src = '/images/' + fileName + '.jpg';
-
-  }, function() {
-    $('#name').removeAttr('style');
   });
 });
