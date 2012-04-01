00d4e08ce5abb9c438cb55217c64bc0bb9ef4524
Alex Crichton <alex@alexcrichton.com>
2012-03-31 23:24:13 -0400

    New site

--- javascripts/application.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($) {
-  $('#name > a:first, #java > a:first, #websites > a:first').click(function(e) {
-    $('#container').attr('class', $(this).parent().attr('id'));
-    e.preventDefault();
-    return false;
-  });
-});
